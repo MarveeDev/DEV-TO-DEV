fc8ac1ff2bc934910d2c87461da27f1ba82e5f2d
MarveeDev <marvee7528@gmail.com>
2026-09-07 20:40:40 +0000

    fix prisma production schema path

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   earlyAccess: true,
+  schema: "apps/api/prisma/schema.prisma",
   datasource: {
     url: process.env.DATABASE_URL || "postgresql://postgres:postgres@postgres:5432/devtodev?schema=public",
   },
